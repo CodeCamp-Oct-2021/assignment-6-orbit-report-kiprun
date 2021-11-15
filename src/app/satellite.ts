c76c51542dd396db44a24edafef3c9e4cc78cb9f
R L <kiprun@gmail.com>
2021-11-14 19:14:07 -0600

    Add warning label

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -15,8 +15,13 @@ export class Satellite {
    }
 	
 	isSpaceDebris(): boolean {
-		return true;
-   }
+		let warningLabel = this.type.toLowerCase();
+		if (warningLabel === "space debris"){
+			return true;
+		};
+				
+	}
+		
 
 }
 
